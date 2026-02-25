a7411bc773e2d19f455a89702f2e688f4ee46c30
Elmael33 <mael.salet33@gmail.com>
2026-02-25 11:38:25 +0100

    [fix] import re library and use re.sub fonction for replace

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
 """filtered logger.py"""
+import re
 
 
 def filter_datum(fields, redaction, message, separator):
     """Filter out specified fields from the message."""
-    parts = message.split(separator)
-    result = []
-
-    for part in parts:
-        if "=" in part:
-            key, value = part.split("=", 1)
-            if key in fields:
-                value = redaction
-            result.append(f"{key}={value}")
-
-    return separator.join(result)
+    for field in fields:
+        message = re.sub(field + '=[^' + separator + ']*', field + '=' + redaction, message)
+    return message
