4a10a4f30b0640df6224ab49bbc361b69cb5451e
Elmael33 <mael.salet33@gmail.com>
2026-02-26 15:21:37 +0100

    [feat] implement a function that returns a connector to the database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,10 @@ filtered_logger.py
 """
 import logging
 import re
+import os
 from typing import List
+import mysql.connector
+
 
 PII_FIELDS: tuple[str] = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -51,3 +54,18 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
     logger.propagate = False
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Get a database connection"""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
