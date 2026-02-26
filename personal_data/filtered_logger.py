1d593deaed564f2891b75e63bcd832a03993b0b3
Elmael33 <mael.salet33@gmail.com>
2026-02-26 10:05:09 +0100

    [fix] pycodestyle linter checked

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,7 +6,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Filter out specified fields from the message.
     """
