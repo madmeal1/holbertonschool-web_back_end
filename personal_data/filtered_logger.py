aa0539a09cb9360e273b098233e68b6d5470a7d0
Elmael33 <mael.salet33@gmail.com>
2026-02-26 14:44:37 +0100

    [fix] define PII_FIELDS is a tuple

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import logging
 import re
 from typing import List
 
-PII_FIELDS = ['name', 'email', 'phone', 'ssn', 'password']
+PII_FIELDS: tuple[str] = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
