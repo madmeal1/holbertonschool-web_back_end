8c236d29d3ae387133ede42234e141a2631c2fcd
Elmael33 <mael.salet33@gmail.com>
2026-02-26 10:04:16 +0100

    [fix] annotated list str parameters

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,10 @@
 filtered logger.py
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Filter out specified fields from the message.
     """
