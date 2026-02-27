475c2893ce448eab7e53390556a25fa47880845e
Elmael33 <mael.salet33@gmail.com>
2026-02-27 13:52:15 +0100

    [feat] implement 403 error forbidden

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized():
     Raises a 401 Unauthorized error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Raise a 403 Forbidden error
+    """
+    abort(403)
