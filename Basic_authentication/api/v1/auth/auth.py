643de84ed5b60e30ce2857e007a7bbfe0cb66b80
Elmael33 <mael.salet33@gmail.com>
2026-02-27 15:06:35 +0100

    [feat] implement conditions in require auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,11 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns False for now"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Returns None for now"""
