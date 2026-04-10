54c312c21368d682c7931776b7677ce41d9221f8
Elmael33 <mael.salet33@gmail.com>
2026-04-10 16:34:36 +0200

    [feat] implement session auth class and update app to start session authentication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -5,8 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -16,12 +15,15 @@ auth = None
 
 auth_type = getenv("AUTH_TYPE")
 
-if auth_type == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-elif auth_type == "auth":
+if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
@@ -46,23 +48,20 @@ def before_request():
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
+def not_found(error):
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Invalid Credential
-    """
+def unauthorized(error):
+    """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """Impossible acces
-    """
+def forbidden(error):
+    """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
