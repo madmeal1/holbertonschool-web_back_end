be12480ce7a4961460486301bdb33e913f410a7a
Elmael33 <mael.salet33@gmail.com>
2026-04-17 14:50:59 +0200

    [feat] add session logout

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import request, jsonify, make_response
 from models.user import User
 from os import getenv
+from flask import abort
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -38,3 +39,15 @@ def session_login():
     )
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def session_logout():
+    """DELETE /api/v1/auth_session/logout"""
+
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
