2b812f7f7f3f709b5a15606025fc4255001c9f6b
Elmael33 <mael.salet33@gmail.com>
2026-04-17 16:51:10 +0200

    [feat] implement uuid generation utility function

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -42,3 +43,7 @@ class Auth:
             )
         except NoResultFound:
             return False
+
+    def _generate_uuid() -> str:
+        """Generate a new UUID string"""
+        return str(uuid.uuid4())
