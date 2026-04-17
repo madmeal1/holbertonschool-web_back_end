00be079902a25e1d9abcd8170f456f316e772964
Elmael33 <mael.salet33@gmail.com>
2026-04-17 15:29:51 +0200

    [feat] implement config project and add user sqlalchemy model

---
+++ user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""User model for authentication service"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User class mapped to the users table."""
+
+    __tablename__ = "users"
+
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: str = Column(String(250), nullable=True)
+    reset_token: str = Column(String(250), nullable=True)
+    
