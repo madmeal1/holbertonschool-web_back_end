956c148cf54d37ce769eb26039c320f7a5d918ab
Elmael33 <mael.salet33@gmail.com>
2026-04-17 13:03:55 +0200

    [feat] implement support session auth in before_request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -33,12 +33,13 @@ def before_request():
         return
 
     excluded = ['/api/v1/status/', '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
+                '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+       auth.session_cookie(request) is None:
         abort(401)
 
     request.current_user = auth.current_user(request)
