ca20bed497e65bbfa1214a6b521378179fd546eb
Elmael33 <mael.salet33@gmail.com>
2026-04-17 10:58:08 +0200

    [feat] implement session cookie from request

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Auth module"""
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -26,3 +27,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None for now"""
         return None
+    
+    def session_cookie(self, request=None):
+        if request is None:
+            return None
+        
+        session_name = getenv("SESSION_NAME")
+        
+        return request.cookies.get(session_name)
