4070d7a98692c87e45fa5053163742769ceaf9e3
Elmael33 <mael.salet33@gmail.com>
2026-04-17 20:52:41 +0200

    [feat] implement update_password method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,3 +85,16 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+    
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update user password using reset token"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        self._db.update_user(
+            user.id,
+            hashed_password=_hash_password(password),
+            reset_token=None
+        )
