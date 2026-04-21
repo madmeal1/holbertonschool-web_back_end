27962d25e9858d6838dad0986aecc87b1ec57eba
madmeal1 <mael.salet33@gmail.com>
2026-04-21 10:51:57 +0200

    [feat] add get_locale function to detect locale from request

---
+++ i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Get locale from request"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determine the best match locale from request"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def index() -> str:
+    """Render the index page"""
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
